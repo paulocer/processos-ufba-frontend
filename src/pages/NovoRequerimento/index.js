6d0f9f969a6b8ce97601c9f2c502c570eb009c5b
Paulo <paulocer@gmail.com>
2019-11-20 16:58:33 -0300

    Alteração da tela de cadastro de requerimentos

--- src/pages/NovoRequerimento/index.js
+++ src/pages/NovoRequerimento/index.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import {
+  AppBar,
+  CssBaseline,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+  useTheme,
+  Box,
+} from '@material-ui/core/'
+
+import {
+  Menu as MenuIcon,
+  Add as AddIcon,
+  History as HistoryIcon,
+  ExitToApp as ExitToAppIcon,
+  SettingsApplications as SettingsApplicationsIcon,
+} from '@material-ui/icons/'
+import {Link} from 'react-router-dom'
+
+import Copyright from '../tail'
+import useStyles from './style'
+import {isUserLogged} from '../../components/Usuario/Usuario'
+
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import {Link} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
 
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
@@ -15,22 +39,23 @@ import FormControl from '@material-ui/core/FormControl';
 import clsx from 'clsx';
 
 import history from '../../history';
-import Copyright from '../tail';
-import useStyles from './style';
 import api from '../../server/config'
 import {Requerimento} from '../../components/Processo/Processo'
-import {isUserLogged} from '../../components/Usuario/Usuario'
 
-
-export default function NovoProcesso(props) {  
-  var { state } = props.location;  
-  const classes = useStyles();  
-  // Valida se o usuário está logado
+export default function NovoProcesso(props) {
+  var { state } = props.location;
+  const { container } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [processos, setProcessos] = useState([]);
+  
+  // Verifica se o usuario está logado
   if(!isUserLogged(state)){
     // Usado para evitar erro ao tentar recuperar a matricula
     state = {matricula: ''};
   }
-
+  
   const [requerimento, setRequerimento] = useState({ ...Requerimento, matricula: state.matricula });
   const validate = (requerimento.esclarecimento === '' || (requerimento.objeto ==='' && requerimento.outro === ''));
 
@@ -69,16 +94,88 @@ function StyledRadio(props) {
   );
 }
 
+  const icons = [<HistoryIcon />, <AddIcon />, <SettingsApplicationsIcon />, <ExitToAppIcon />]
+  const caminho = [{ pathname: '/home', state: state }, { pathname: '/novorequerimento', state: state }, { pathname:'/cadastro', state: state }, { pathname:'/'}]
+
+ 
+
+
+  // 
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <List>
+        {['Requerimentos', 'Novo Requerimento', 'Alteração Cadastral', 'Sair'].map((text, index) => (
+          //<Link href={index === 0 ? "#" : index === 1 ? "#" : index === 2 ? "#" : index === 3 ? "/" : index === 4 ? "/recupera" : "/cadastro"} className={classes.link}>
+          <Link to={caminho[index]} className={classes.link} style={{ color: "#000", textDecoration: "none" }}>
+            <ListItem button key={text}>
+              {/* <ListItemIcon>{index === 0 ? <HomeIcon /> : index === 1 ? <HistoryIcon /> : index === 2 ? <AddIcon /> : <WarningIcon />}</ListItemIcon> */}
+              <ListItemIcon>{icons[index]}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </div>
+  );
 
   return (
-    <Container component="main" maxWidth="xs">
+    <div className={classes.root}>
       <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          NOVO REQUERIMENTO
-        </Typography>
-        <form className={classes.form} noValidate onSubmit= {handleSubmit}>
-        <h2 component="legend">Data</h2>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            //onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            NOVO REQUERIMENTO
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            //onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true,
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+      <form className={classes.form} noValidate onSubmit= {handleSubmit}>
+      <h2 component="legend">Data</h2>
           <TextField
             id="date"
             type="date"
@@ -148,10 +245,8 @@ function StyledRadio(props) {
           </Button>
           
         </form>
-      </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
-    </Container>
+
+      </main>
+    </div>
   );
 }
--- src/pages/NovoRequerimento/style.js
+++ src/pages/NovoRequerimento/style.js
@@ -1,60 +1,85 @@
-
 import { makeStyles } from '@material-ui/core/styles';
 
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
-    '@global': {
-      body: {
-        backgroundColor: theme.palette.common.white,
-      },
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
     },
-    paper: {
-      marginTop: theme.spacing(4),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
     },
-    
-    form: {
-      width: '200%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
     },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  paper: {
+    marginTop: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  
+  form: {
+    width: '200%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  icon: {
+    borderRadius: '50%',
+    width: 16,
+    height: 16,
+    boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+    backgroundColor: '#f5f8fa',
+    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+    '$root.Mui-focusVisible &': {
+      outline: '2px auto rgba(19,124,189,.6)',
+      outlineOffset: 2,
     },
-    icon: {
-      borderRadius: '50%',
+    'input:hover ~ &': {
+      backgroundColor: '#ebf1f5',
+    },
+    'input:disabled ~ &': {
+      boxShadow: 'none',
+      background: 'rgba(206,217,224,.5)',
+    },
+  },
+  checkedIcon: {
+    backgroundColor: '#137cbd',
+    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+    '&:before': {
+      display: 'block',
       width: 16,
       height: 16,
-      boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
-      backgroundColor: '#f5f8fa',
-      backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
-      '$root.Mui-focusVisible &': {
-        outline: '2px auto rgba(19,124,189,.6)',
-        outlineOffset: 2,
-      },
-      'input:hover ~ &': {
-        backgroundColor: '#ebf1f5',
-      },
-      'input:disabled ~ &': {
-        boxShadow: 'none',
-        background: 'rgba(206,217,224,.5)',
-      },
+      backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
+      content: '""',
     },
-    checkedIcon: {
-      backgroundColor: '#137cbd',
-      backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
-      '&:before': {
-        display: 'block',
-        width: 16,
-        height: 16,
-        backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
-        content: '""',
-      },
-      'input:hover ~ &': {
-        backgroundColor: '#106ba3',
-      },
+    'input:hover ~ &': {
+      backgroundColor: '#106ba3',
     },
-  }));
+  },
 
-  export default useStyles;
+}));
+
+export default useStyles;
